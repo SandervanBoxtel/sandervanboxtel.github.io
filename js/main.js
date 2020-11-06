ef9ca34bf20b1e71cf0344528414ea1b54c657d4
sandervanboxtel <sandervanboxtel@ziggo.nl>
2020-11-06 11:56:43 +0100

    Update Whole Website Project
    Update Website Project Light & Dark Mode

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', (event) => {
+    if (event.target.checked === true) {
+        document.getElementById('mystylesheet').href = "/css/sander-style-dark.css";
+        document.getElementById('logonav').src = "/img/SanderLogo.png"
+    }
+    else {
+        document.getElementById('mystylesheet').href = "/css/sander-style.css";
+        document.getElementById('logonav').src = "/img/SanderLogoBlue.png"
+    }
+});
+
